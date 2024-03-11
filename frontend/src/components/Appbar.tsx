ef3cd629a7504e518be5a8911cdf27f7506add2b
SanketDhabarde <sanketdhabarde13@gmail.com>
2024-03-11 17:05:03 +0530

    style: appbar

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -12,19 +12,27 @@ const Appbar = () => {
       <Link to={"/blogs"} className="font-bold">
         Medium
       </Link>
-      <div className="flex gap-3">
-        <button
-          onClick={handleLogout}
-          className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5"
-        >
+      <div className="flex justify-center items-center gap-6">
+        <Link to={"/create"} className="flex gap-1">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+            />
+          </svg>
+          Write
+        </Link>
+        <button onClick={handleLogout}>
           logout
         </button>
-        <Link
-          to={"/create"}
-          className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5"
-        >
-          Create
-        </Link>
         <div
           className={`relative inline-flex items-center justify-center h-8 w-8 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600`}
         >
